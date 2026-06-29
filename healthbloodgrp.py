ccd5d359027fa734ddfbef00f1fbdf291d31d3d9
Amit-Karki <iiswiikar@gmail.com>
2026-06-29 21:24:25 +0545

    Comments added in agenpatienthealthcare.py file and healthbloodgrp.py fixed slight syntax error

--- healthbloodgrp.py
+++ healthbloodgrp.py
@@ -4,6 +4,7 @@ pd.set_option('display.max_rows',None)
 pd.set_option('display.max_columns',None)
 df = pd.read_csv('healthcare_dataset.csv')
 print(df.columns)
+
 blood_typ = df.groupby('Blood Type')['Test Results'].count()
 blood_typ.sort_values(ascending=False,inplace=True)
 print('Here is a list which shows number of patients based on the blood group')
